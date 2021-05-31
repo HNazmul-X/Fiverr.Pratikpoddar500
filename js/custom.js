5c48ff1e5298eef37cd3f42ce399bfd8a9a8604e
H Nazmul Hassan <71649134+HNazmul@users.noreply.github.com>
2021-05-31 18:45:56 +0600

    toggle added on first qize slider

--- js/custom.js
+++ js/custom.js
@@ -18,10 +18,31 @@ togglerIcon.addEventListener("click", () => {
     togglerContent.classList.toggle("left");
 });
 
-
 /* quize card mbl responsive text shower */
-const mblviewedText = document.querySelectorAll("#quizes-card .for-mbl-view-text-SWSHTSHT"); 
-const mblviewedTextComeUp = document.querySelectorAll("#quizes-card .come-up-text"); 
+const mblviewedText = document.querySelectorAll("#quizes-card .for-mbl-view-text-SWSHTSHT");
+const mblviewedTextComeUp = document.querySelectorAll("#quizes-card .come-up-text");
 mblviewedText.forEach((eachNode, index) => {
-    eachNode.innerHTML = mblviewedTextComeUp[index].innerHTML
-})
+    eachNode.innerHTML = mblviewedTextComeUp[index].innerHTML;
+});
+
+/* quize card toogle code */
+// width of canceler
+const quizesitebarCanceler_SDDHT = document.querySelector('main#page_2 #carousel-1_2 [id*="catagory_"] .toogle___QUIZE_SITEBAR .toogle-content .content-wrapper .toggle-content-canceler');
+const quzesitebarContent_Sksnsa5t = document.querySelector('main#page_2 #carousel-1_2 [id*="catagory_"] .toogle___QUIZE_SITEBAR .toogle-content .content-wrapper .content');
+
+const resizingCancelerHeight = () => {
+    if (quizesitebarCanceler_SDDHT && quzesitebarContent_Sksnsa5t) {
+        quizesitebarCanceler_SDDHT.style.height = quzesitebarContent_Sksnsa5t.clientHeight + "px";
+    }
+};
+
+const quizeSiteBarToogleIcon = document.querySelector('main#page_2 #carousel-1_2 [id*="catagory_"] .toogle___QUIZE_SITEBAR .toogle-icon');
+const quizeSitebarToogleContent = document.querySelector('main#page_2 #carousel-1_2 [id*="catagory_"] .toogle___QUIZE_SITEBAR .toogle-content');
+quizeSiteBarToogleIcon.addEventListener("click", () => {
+    resizingCancelerHeight();
+    quizeSitebarToogleContent.classList.toggle("view");
+});
+
+quizesitebarCanceler_SDDHT.addEventListener("click", () => {
+    quizeSitebarToogleContent.classList.toggle("view");
+});
