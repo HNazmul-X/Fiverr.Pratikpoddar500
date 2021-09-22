cec5cb8b0a899f6cd3ecae3d7863cca9266b2ab3
H. Nazmul Hassan <nazmul.w3@gmail.com>
2021-09-23 02:07:15 +0600

    why learn edu section and more 3 section added

--- second time/js/index.js
+++ second time/js/index.js
@@ -1,15 +1,12 @@
-
 /* Vanila tilt.js */
 VanillaTilt.init(document.querySelectorAll(".tiltcard"), {
     max: 10,
     speed: 400,
-    glare:true
+    glare: true,
 });
 
-
-
 /* navbar code */
-const navbar = document.querySelector("nav#navbar")
+const navbar = document.querySelector("nav#navbar");
 
 const navbarDropdownAtMobile = () => {
     if (window.innerWidth <= 800) {
@@ -28,20 +25,24 @@ const navbarDropdownAtMobile = () => {
             };
         });
     }
+};
 
+try {
     const navbarToggler = document.querySelector(".navbar-toggler");
     const navbarItemsContainer = document.querySelector(".navbar-items-container");
     navbarToggler.addEventListener("click", () => {
         navbarItemsContainer.classList.add("active");
         navbarCloser.classList.toggle("active");
     });
-    
+
     const navbarCloser = document.querySelector(".container-closer");
     navbarCloser.addEventListener("click", () => {
         navbarItemsContainer.classList.remove("active");
         navbarCloser.classList.toggle("active");
     });
-};
+} catch (e) {
+    console.error(e);
+}
 
 window.addEventListener("resize", () => {
     navbarDropdownAtMobile();
@@ -49,22 +50,68 @@ window.addEventListener("resize", () => {
 
 navbarDropdownAtMobile();
 
-
 window.addEventListener("scroll", () => {
-    if(window.scrollY >=45){
+    if (window.scrollY >= 45) {
         navbar.style.cssText = `
         background: var(--green-dark);
         `;
     } else {
-        navbar.style.cssText = null
+        navbar.style.cssText = null;
     }
-})
-
+});
 
 /* Glidejs carousel */
-var glide = new Glide("#our-result-carousel", {
+const glide = new Glide("#our-result-carousel", {
     type: "carousel",
     perView: 1,
 });
 
 glide.mount();
+
+/* swiper js carousel for discover carousel */
+var swiper = new Swiper("#discover-slider", {
+    // effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    loop: true,
+    autoplay: false,
+    autoHeight: true,
+    breakpoints: {
+        440: {
+            slidesPerView: 1,
+            spaceBetween: 0,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: -20,
+        },
+    },
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+    },
+});
+
+try {
+    const allDiscoverSliderPlayIcons = document.querySelectorAll(".discover-card .play-icon");
+
+    //  createing bootstrap Modal instance
+    const discoverModalElement = document.getElementById("discover-slider-videoShower");
+    const discoverSliderModal = new bootstrap.Modal(discoverModalElement, {
+        keyboard: false,
+    });
+
+    allDiscoverSliderPlayIcons.forEach((playIcon) => {
+        playIcon.addEventListener("click", () => {
+            discoverSliderModal.show();
+            console.log(discoverModalElement);
+            console.log(discoverSliderModal);
+        });
+    });
+} catch (e) {
+    console.error(e);
+}
